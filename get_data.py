a1facf16c1a951ba5f06969f6a190f4e88cbf9a8
vitoraugustx <vitor.arp2010@gmail.com>
2023-06-06 14:46:17 -0300

    Alteração no get_data

--- get_data.py
+++ get_data.py
@@ -9,20 +9,20 @@ from pathlib import Path
 https://chrome.google.com/webstore/detail/online-dino/ckkofollclpnogccmelmlekkcgnanphc?hl=tr
 """
 
-frame = {"top":298, "left":185, "width":408, "height":408} # Borders of the screenshot
+frame = {"top":300, "left":295, "width":360, "height":400} # Borders of the screenshot
 ss_manager = mss()  # We are using mss() for taking a screenshot
 count = 0           # A variable which count the screenshots
 is_exit = False     # A variable for stopping the program
 
 
 # A function for taking a screenshot
-def take_screenshot(ss_id, key):
+def take_screenshot(ss_id, key, path="./images/"):
     global count
     count += 1
     print("{}: {}".format(key, count))
     img = ss_manager.grab(frame)
     image = Image.frombytes("RGB", img.size, img.rgb)
-    image.save("./images/{}_{}_{}.png".format(key, ss_id, count))
+    image.save(path +"{}_{}_{}.png".format(key, ss_id, count))
     time.sleep(1.5)
 
 
@@ -35,6 +35,9 @@ def exit():
 # MAIN PROGRAM
 if __name__ == '__main__':
     Path("./images/").mkdir(parents=True, exist_ok=True) # Create images directory if not exist
+    Path("./images/up").mkdir(parents=True, exist_ok=True) # Create images directory if not exist
+    Path("./images/down").mkdir(parents=True, exist_ok=True) # Create images directory if not exist
+    Path("./images/none").mkdir(parents=True, exist_ok=True) # Create images directory if not exist
     keyboard.add_hotkey("esc", exit)    # If user clik the 'esc', the program will stop
     ss_id = uuid.uuid4()                # An id for all screenshots
 
@@ -44,13 +47,13 @@ if __name__ == '__main__':
 
         try:
             if keyboard.is_pressed(keyboard.KEY_UP):        # If 'up' key is pressed
-                take_screenshot(ss_id, "up")
+                take_screenshot(ss_id, "up", path="./images/up/")
                 time.sleep(0.01)
             elif keyboard.is_pressed(keyboard.KEY_DOWN):    # If 'down' key is pressed
-                take_screenshot(ss_id, "down")
+                take_screenshot(ss_id, "down", path="./images/down/")
                 time.sleep(0.01)
             elif keyboard.is_pressed("right"):              # If 'right' key is pressed
-                take_screenshot(ss_id, "right")
+                take_screenshot(ss_id, "right", path="./images/none/")
                 time.sleep(0.01)
         except RuntimeError: 
             continue
