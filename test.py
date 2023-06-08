4b90dcb098892d6051f89fc13b3765de5acd730b
Vitor <vitor.arp2010@gmail.com>
2023-06-08 12:35:46 -0300

    funcional

--- test.py
+++ test.py
@@ -1,31 +1,55 @@
-import cv2
-import time
+import glob
+import os
+from PIL import Image
 import numpy as np
-from mss import mss
 
-def draw_bboxes(img, bboxes, color=(0, 0, 255), thickness=2):
-    for bbox in bboxes:
-        return cv2.rectangle(img, tuple(bbox[:2]), tuple(bbox[:2]+bbox[-2:]), color, thickness)
+width = 100     # Width of all images
+height = 100    # Height of all images
 
-# bounding boxes
-bboxes = [np.array([300, 295, 360, 400])]
+# Get all images and labels from base path subdivided by class
+def get_images_and_labels(base_path):
+    X = []      # A list for store the images
+    Y = []      # A list for store the labels
 
-with mss() as sct:
-    # part of the screen to capture
-    monitor = {"top":0, "left":0, "width":1920, "height":1080}
-    
-    # get screen
-    screen = np.asarray(sct.grab(monitor))
-    
-    # convert from BGRA --> BGR
-    screen = cv2.cvtColor(screen, cv2.COLOR_BGRA2BGR)
-    # draw bboxes
-    screen = draw_bboxes(screen, bboxes)
-    resized = cv2.resize(screen, (1366, 768))
+    for class_path in glob.glob(base_path + "/*"):    # Get all class paths from base path
+        label = os.path.basename(class_path)          # Get label from class path
+        for img_path in glob.glob(class_path + "/*"): # Get all image paths from class path
+            grey_image = Image.open(img_path).convert("L")      # Convert RGB image to grey_scale image
+            img = np.array(grey_image.resize((width, height)))  # Resize the grey image and convert it to numpy array
+            img = img / 255                                     # Normalize the image array
+            X.append(img)                                       # Append image array to X
+            Y.append(label)                                     # Append label to Y
 
-    # display
-    cv2.imshow("OpenCV/Numpy normal", resized)
-    cv2.waitKey()
-    
-        
+    X = np.array(X)                                         # Convert list X to numpy array
+    X = X.reshape(X.shape[0], width, height, 1)             # Reshape the X
+
+    # return to_categorical(Y), Y
+    from keras.utils import to_categorical
+    label_encoder = LabelEncoder()
+    integer_encoded = label_encoder.fit_transform(Y)
+    return X, integer_encoded
+
+
+    return X, Y
+
+# Get base path
+base_path = "images"
+
+# Get images and labels
+X, Y = get_images_and_labels(base_path)
+print(X.shape)
+
+
+# Dict label_encoder classes
+label_encoder = LabelEncoder()
+integer_encoded = label_encoder.fit_transform(Y)
+print(integer_encoded)
+
+# Convert labels to onehot labels
+from keras.utils import to_categorical
+onehot_labels = to_categorical(integer_encoded)
+print(onehot_labels)
+
+# Split dataset to train and test
+
    
